1816ec5336ef078d3bf0ca438d9f5dd88aa4bc0c
mahln908 <mazinholn18@gmail.com>
2025-08-01 23:22:16 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,8 +1,12 @@
-// Service Worker para permitir reprodução em background
+// Service Worker para tentar manter a reprodução em background
 self.addEventListener('install', (event) => {
     event.waitUntil(self.skipWaiting());
 });
 
 self.addEventListener('activate', (event) => {
     event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', (event) => {
+    // Pode adicionar estratégias de cache aqui se necessário
 });

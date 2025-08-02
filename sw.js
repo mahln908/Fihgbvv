535ce885f100e5aa36101cd4e58fe33bef3e379d
mahln908 <mazinholn18@gmail.com>
2025-08-01 22:58:23 -0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,8 @@
+// Service Worker para permitir reprodução em background
+self.addEventListener('install', (event) => {
+    event.waitUntil(self.skipWaiting());
+});
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(self.clients.claim());
+});

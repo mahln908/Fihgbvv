f3b6782a90e5a9be75937a9494cdb7792e7bf1d3
mahln908 <mazinholn18@gmail.com>
2025-08-03 20:06:25 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,12 +1,33 @@
-// Service Worker para tentar manter a reprodução em background
-self.addEventListener('install', (event) => {
-    event.waitUntil(self.skipWaiting());
+// Service Worker for notifications and offline support
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open('gospel-radio-v1').then(cache => {
+            return cache.addAll([
+                '/',
+                '/index.html',
+                'https://i.imgur.com/JNZF5Bw.jpg'
+            ]);
+        })
+    );
 });
 
-self.addEventListener('activate', (event) => {
-    event.waitUntil(self.clients.claim());
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request);
+        })
+    );
 });
 
-self.addEventListener('fetch', (event) => {
-    // Pode adicionar estratégias de cache aqui se necessário
+self.addEventListener('push', event => {
+    const data = event.data.json();
+    const options = {
+        body: data.body,
+        icon: 'https://i.imgur.com/JNZF5Bw.jpg',
+        badge: 'https://i.imgur.com/JNZF5Bw.jpg'
+    };
+    
+    event.waitUntil(
+        self.registration.showNotification(data.title, options)
+    );
 });
